347c43be31b3ea92cfb4f6b7f2eba320f3a61b9b
Khoa Nguyen <nguyen.khoa@clublabs.com>
2021-11-10 11:09:55 -0800

    fix default urls

--- web/src/services/endpoints.ts
+++ web/src/services/endpoints.ts
@@ -1,6 +1,8 @@
 export let ROOT_URL = '';
 if (process.env.NODE_ENV === 'development') {
  ROOT_URL = 'http://localhost:8080';
+} else {
+  console.log('leaving default url', ROOT_URL)
 }
 export default {
 
